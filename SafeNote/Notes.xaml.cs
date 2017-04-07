9ac106f59ddc75f6da81980af2274f55614423c9
taraokelly <taraokelly.dev@gmail.com>
2017-04-07 22:36:53 +0100

    Add Settings Button and Save Individual Note Functionality

--- SafeNote/Notes.xaml.cs
+++ SafeNote/Notes.xaml.cs
@@ -23,6 +23,7 @@ namespace SafeNote
         StorageFolder localFolder = ApplicationData.Current.LocalFolder;
         StorageFile notes;
         string fileName = "notes.txt";
+        Note n = new Note();
 
         public Notes()
         {
@@ -39,14 +40,16 @@ namespace SafeNote
             }
 
         }
-        public async void saveNote()
+        public async void saveNote(string title, string body)
         {
             notes = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
 
-            foreach (var item in dataList)
-            {
-                await FileIO.AppendTextAsync(notes, item.title + Environment.NewLine + item.body + Environment.NewLine);
-            }
+            n = new Note() { title = title, body = body };
+
+            dataList.Add(n);
+
+            await FileIO.AppendTextAsync(notes, title + Environment.NewLine + body + Environment.NewLine);
+            
 
         }
         public async void loadNotes()
@@ -59,26 +62,24 @@ namespace SafeNote
             dataList.Add(n2);
             dataList.Add(n3);*/
 
-                   notes = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
-                  var info = await FileIO.ReadLinesAsync(notes);
-                   int count = 0;
-                   Note n = new Note();
+            notes = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+            var info = await FileIO.ReadLinesAsync(notes);
+            int count = 0;
 
-                   foreach (var line in info)
-                   {
-                       if (count % 2 == 0)
-                       {
-                            n = new Note();
-                            n.title = line;
-                       }
-                       else
-                       {
-                            n.body = line;
-                            dataList.Add(n);
-                           
-                       }
-                       count++;
-                   }
+            foreach (var line in info)
+            {
+                if (count % 2 == 0)
+                {
+                    n = new Note();
+                    n.title = line;
+                }
+                else
+                {
+                    n.body = line;
+                    dataList.Add(n);       
+                }
+                count++;
+            }
 
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -86,6 +87,11 @@ namespace SafeNote
             listView.ItemsSource = dataList;
             loadNotes();
             //saveNotes();
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 
